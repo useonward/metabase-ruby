e33406de35cc1ecceb076efbb2fcd305aa746871
Dmitry L Rocha <dmitryrck@gmail.com>
2024-02-15 21:06:31 -0400

    Add VCR configuration to force encoding and pretty generate JSON response

--- spec/support/vcr.rb
+++ spec/support/vcr.rb
@@ -6,4 +6,11 @@ VCR.configure do |config|
   config.cassette_library_dir = 'spec/vcr_cassettes'
   config.hook_into :webmock
   config.configure_rspec_metadata!
+  config.before_record do |i|
+    i.response.body.force_encoding("UTF-8")
+
+    if i.response.headers["Content-Type"][0].include?("application/json") && i.response.body.size > 0
+      i.response.body = JSON.pretty_generate(JSON.parse(i.response.body))
+    end
+  end
 end
